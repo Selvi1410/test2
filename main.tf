eb9e883d742c07ce36a31bdc86fdc0c23dd514a3
Selvi1410 <88480364+Selvi1410@users.noreply.github.com>
2022-09-27 16:11:15 +0800

    Update main.tf

--- main.tf
+++ main.tf
@@ -16,15 +16,6 @@ resource "aws_instance" "myec2" {
   instance_type = var.instance_type
  
 
-  tags = {
-    aws_instance_type = var.aws_instance_type
-    Environment = var.Environment
-    Environment_type = var.Environment_type
-    Number_of_Instances = var.ec2_instance_count
-    Resource_Type_BlockStorage = var.Resource_Type_BlockStorage
-    Storage_type = var.Storage_type
-    
-    
-  }
+
 }
 
