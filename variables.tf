71b4e15b99775bfcc2b983d3c4958837037ed4a0
Selvi1410 <88480364+Selvi1410@users.noreply.github.com>
2022-10-03 13:27:50 +0800

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -10,7 +10,7 @@ variable "ec2_ami_id" {
 
 variable "instance_type" {
   description = "type of EC2 instance to provision."
-  default = "t2.small"
+  default = "c5.2xlarge"
 }
 
 variable "ec2_instance_name" {
